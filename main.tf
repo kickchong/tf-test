d39ce3035fea50125b0be805cf4cd56cf1f34924
kickchong <kickchong@gmail.com>
2021-11-15 12:42:23 -0800

    4

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "tfcore"
+
+    workspaces {
+      name = "my-workspace"
+    }
+  }
+}
+
 variable "user1" {
     type = string
   
