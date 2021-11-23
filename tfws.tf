d67f99f21d98269e2ccd7441071b8e96862b73f3
kickchong <kickchong@gmail.com>
2021-11-23 12:08:19 -0800

    create workspace

--- tfws.tf
+++ tfws.tf
@@ -2,7 +2,6 @@ resource "tfe_workspace" "myworkspace" {
   name              = "myworkspace"
   organization      = "stamp-test"
   execution_mode    = "remote"
-  working_directory = ""
   auto_apply        = "false"
   queue_all_runs    = "false"
   tag_names         = ["myworkspace"]
