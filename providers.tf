ed01b2b99d635fe106956cb36b89311adba8b6a8
kickchong <kickchong@gmail.com>
2021-11-29 12:51:38 -0800

    c3

--- providers.tf
+++ providers.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.64.2"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.26.1"
     }
   }
 }
