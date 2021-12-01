0507af646e7f0d5676b8d31a017a6475554c9586
kickchong <kickchong@gmail.com>
2021-12-01 07:58:30 -0800

    create workspace

---
+++ tfws.tf
@@ -0,0 +1,21 @@
+resource "tfe_workspace" "workspace" {
+  name            = "workspace"
+  organization    = "stamps-test"
+  execution_mode  = "remote"
+}
+
+# resource "tfe_workspace" "myworkspace" {
+#   name              = "myworkspace1121"
+#   organization      = "stamp-test"
+#   execution_mode    = "remote"
+#   auto_apply        = "false"
+#   queue_all_runs    = "false"
+#   tag_names         = ["myworkspace 11/2021"]
+
+#   vcs_repo {
+#     identifier     = "kickchong/tf-test"
+#     branch         = "main"
+#     oauth_token_id = "ot-MnZsfdRyaDZ9Z2MU"
+#   }
+  
+# }
